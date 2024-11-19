b7fb6fca54939e3a3813b072a6a5db6957b3b772
Max Seelig <maxseelig@gmail.com>
2024-11-19 17:28:43 +0100

    Install GSAP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import { gsap } from 'gsap';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        gsap.to('h1', { x: 100, duration: 1 });
+    });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
