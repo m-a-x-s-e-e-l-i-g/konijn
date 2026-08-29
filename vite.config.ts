3301505a990e08434e99135dbf99972af6eddd89
Max Seelig <7907436+m-a-x-s-e-e-l-i-g@users.noreply.github.com>
2026-08-29 11:02:17 +0200

    fix: allow Tailscale development host

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		allowedHosts: ['desktop-hucdjae.tailcb2363.ts.net']
+	},
 	plugins: [sveltekit()]
 });
